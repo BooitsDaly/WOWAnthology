95d49e83ac1db0f9c1ac22894a356c70fb5050fb
Boots <cnd9351@rit.edu>
2018-12-13 12:26:31 -0500

    Initial Commit

---
+++ WOWAnthology/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  WOWAnthology
+//
+//  Created by Caitlyn Daly on 12/13/18.
+//  Copyright © 2018 Caitlyn Daly. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
